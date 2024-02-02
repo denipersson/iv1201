fe5844e887185561ee0f04e6d304a6c5088fa148
Deni Persson <39971336+denipersson@users.noreply.github.com>
2024-02-02 18:54:07 +0100

    made exception for port :3001

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import { loginPerson, registerPerson } from './controllers/personController';
 import { validateLogin, validateRegistration } from './middleware/validate';
 import * as dotenv from 'dotenv';
 
+const cors = require('cors');
+
+
+const corsOptions = {
+  origin: 'http://localhost:3001', // This should match the domain of your frontend application
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 dotenv.config(); // Load environment variables from .env file, which will be ignored by Git. In this file we will store sensitive information like database credentials and JWT secret key
 
 const app: Application = express();
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.post('/register', validateRegistration, registerPerson);
