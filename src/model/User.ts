6eaa4a49adb4d924c6955e74bfb7e5671c841320
Deni Persson <39971336+denipersson@users.noreply.github.com>
2024-02-06 15:46:45 +0100

    Update User.ts

--- src/model/User.ts
+++ src/model/User.ts
@@ -12,14 +12,7 @@ export interface User {
 
 // Set User's password to '' when sending it to the client
 export const sanitizeUser = (user: User): User => {
-    return {
-        person_id: user.person_id,
-        name: user.name,
-        surname: user.surname,
-        pnr: user.pnr,
-        email: user.email,
-        password: '',
-        role_id: user.role_id,
-        username: user.username
-    };
+    const sanitizedUser = user;
+    sanitizedUser.password = '';
+    return sanitizedUser;
 };
