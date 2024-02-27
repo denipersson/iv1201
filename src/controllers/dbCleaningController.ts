443431dd948ed40bf08cb8b70f584bd83c88e408
3kguccisuit <104920236+3kguccisuit@users.noreply.github.com>
2024-02-27 16:37:26 +0100

    using console logs

--- src/controllers/dbCleaningController.ts
+++ src/controllers/dbCleaningController.ts
@@ -21,23 +21,21 @@ export const getUsersWithBadData = async (type: string) => {
                 await emailPeopleWithNoUsername(users);
                 break;
             default:
-                return //res.status(400).send({ message: 'Invalid query parameter value for type. Please use one of the following: password, email, username.' });
+                return "Invalid query parameter value for type. Please use one of the following: password, email, username.";
         }
 
         if (users.length > 0) {
             if (type == 'unencrypted')
                 console.log(`Successfully found users with ${type} issues and changed ${users.length} users`);
-            //res.status(200).send({ message: `Successfully found users with ${type} issues and changed ${users.length} users` });
             else
                 console.log(`Successfully found users with ${type} issues and notified ${users.length}`);
-            // res.status(200).send({ message: `Successfully found users with ${type} issues and notified ${users.length}` });
         }
         else
         console.log(`No users found  with ${type} issues`);
-           // res.status(200).send({ message: `No users found  with ${type} issues` });
+
     } catch (error) {
         console.error(`Error getting users with issues:`, error);
-        // res.status(500).send({ message: `Failed to get users with ${req.query.type} issues` });
+
     }
 };
 
