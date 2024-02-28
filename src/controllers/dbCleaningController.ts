31f3b3b1af1d1a9b2928eb9a1d6157d98f5baf92
3kguccisuit <104920236+3kguccisuit@users.noreply.github.com>
2024-02-28 19:59:32 +0100

    Check existing db (#46)
    * Check for password, email, username, unencrypted
    
    * using console logs

---
+++ src/controllers/dbCleaningController.ts
@@ -0,0 +1,42 @@
+import { findUsersWithNoPassword, findUsersWithoutEmail, findUsersWithoutUsernames, findUsersWithUnencryptedPassword, encryptUnencryptedPasswords, emailPeopleWithNoUsername, emailPasswordResetLinks } from '../dao/databaseCleaningDAO';
+
+export const getUsersWithBadData = async (type: string) => {
+    try {
+        let users = [];
+        // Determine the type of issue and call the appropriate DAO function
+        switch (type) {
+            case 'password':
+                users = await findUsersWithNoPassword();
+                await emailPasswordResetLinks(users);
+                break;
+            case 'unencrypted':
+                users = await findUsersWithUnencryptedPassword();
+                await encryptUnencryptedPasswords(users);
+                break;
+            case 'email':
+                users = await findUsersWithoutEmail();
+                break;
+            case 'username':
+                users = await findUsersWithoutUsernames();
+                await emailPeopleWithNoUsername(users);
+                break;
+            default:
+                return "Invalid query parameter value for type. Please use one of the following: password, email, username.";
+        }
+
+        if (users.length > 0) {
+            if (type == 'unencrypted')
+                console.log(`Successfully found users with ${type} issues and changed ${users.length} users`);
+            else
+                console.log(`Successfully found users with ${type} issues and notified ${users.length}`);
+        }
+        else
+        console.log(`No users found  with ${type} issues`);
+
+    } catch (error) {
+        console.error(`Error getting users with issues:`, error);
+
+    }
+};
+
+
