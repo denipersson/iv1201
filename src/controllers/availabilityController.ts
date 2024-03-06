f197de42f2e8fa2b220d965a69469d5bde6f7a72
Edvin Frosterud <edvin.frosterud@gmail.com>
2024-03-06 18:56:41 +0100

    getAvailability
    + endpoint for single target getAvailibility
    + Inject avalability in getApplicants.

--- src/controllers/availabilityController.ts
+++ src/controllers/availabilityController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
-import { addAvailabilityForPerson } from '../dao/availabilityDAO'
+import { addAvailabilityForPerson, getAvailabilityForPersonByUsername } from '../dao/availabilityDAO'
 
+/**
+ * Adds availability for a person.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 export const addAvailability = async (req: Request, res: Response) => {
     const { fromDate, toDate } = req.body;
     const personId = res.locals.personId;
@@ -16,5 +21,27 @@ export const addAvailability = async (req: Request, res: Response) => {
     } catch (error) {
         console.error('Error during applicant retrieval:', error);
         res.status(500).json('An error occurred during adding availability');
+    }
+};
+
+/**
+ * Retrieves availability for a person by username.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
+export const getAvailability = async (req: Request, res: Response) => {
+    const { username } = req.body;
+
+    try {
+        if (username === undefined) {
+            throw new Error("No personId provided");
+        }
+
+        const availability = await getAvailabilityForPersonByUsername(username);
+        res.status(200).json(availability);
+
+    } catch (error) {
+        console.error('Error during availability retrieval:', error);
+        res.status(500).json('An error occurred during availability retrieval');
     }
 };
